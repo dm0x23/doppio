d3121831a4c315b51b64694bb44d5ed3c14af2ea
dm0x23 <denisha.madhura2007@gmail.com>
2026-04-22 16:46:47 +0530

    Implemented the list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,40 +1,40 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/dm0x23/doppio/internal/storage"
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
-	},
+	Short: "List all shortcuts",
+	Long:  `Display all shortcuts currently managed by doppio`,
+	RunE:  runList,
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func runList(cmd *cobra.Command, args []string) error {
+	shortcuts, err := storage.List()
+	if err != nil {
+		return fmt.Errorf("failed to load shortcuts: %w", err)
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+	if len(shortcuts) == 0 {
+		fmt.Println("No shortcuts added")
+		fmt.Println("Add a shortcut using dop add <name> <command>")
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	fmt.Println("shortcuts")
+
+	for _, s := range shortcuts {
+		fmt.Printf("%s -> %s\n", s.Name, s.Command)
+	}
+
+	return nil
 }
