4f8843aa6befb003a68ae9b71a5a583628a86f94
den <denisha.madhura2007@gmail.com>
2026-04-23 23:47:30 +0530

    implemented the bash adapter in bash.go

---
+++ internal/shell/bash.go
@@ -0,0 +1,21 @@
+package shell
+
+import (
+	"os"
+	"path/filepath"
+)
+
+type BashAdapter struct{}
+
+func (b BashAdapter) GenerateAlias(name, command string) string {
+	return `alias` + name + `="` + command + `"`
+}
+
+func (b BashAdapter) RCFilePath() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".bashrc")
+}
+
+func (b BashAdapter) ManagedBlockMarkers() (string, string) {
+	return "# >>> doppio managed >>>", "# <<< doppio managed <<<"
+}
